79b36027c9550840fcf2a090c980f993b0af2f02
parekhakhil <39043468+parekhakhil@users.noreply.github.com>
2020-09-02 10:52:59 +0530

    authentication added

--- authdemo/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\parek\onedrive\desktop\projects\projects\djangoauthapp\authdemo\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
